f878be85567ebb24c980e1b98b29ba1f9c334380
siddiqa-malik <siddiqamalik888@gmail.com>
2026-02-01 22:24:40 +0500

    remove listen for vercel

--- server.js
+++ server.js
@@ -26,8 +26,4 @@ mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
-});
+export default app;
