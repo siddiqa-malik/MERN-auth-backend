ba8947514f5189d1bebbdfebbf52a05cc068ef63
siddiqa-malik <siddiqamalik888@gmail.com>
2026-02-01 23:51:41 +0500

    update server.js

--- server.js
+++ server.js
@@ -1,3 +1,33 @@
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const dotenv = require("dotenv");
+// const cors = require("cors");
+
+// dotenv.config();
+
+// const authRoutes = require("./routes/authRoutes");
+// const userRoutes = require("./routes/userRoutes");
+
+// const app = express();
+
+// /* 🔥 CORS FIX */
+// app.use(cors({
+//   origin: "*",
+//   methods: ["GET","POST","PUT","DELETE"],
+//   allowedHeaders: ["Content-Type","Authorization"]
+// }));
+
+// app.use(express.json());
+
+// app.use("/api/auth", authRoutes);
+// app.use("/api/users", userRoutes);
+
+// mongoose.connect(process.env.MONGO_URI)
+// .then(() => console.log("MongoDB Connected"))
+// .catch(err => console.log(err));
+
+// export default app;
+
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
@@ -19,11 +49,14 @@ app.use(cors({
 
 app.use(express.json());
 
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
+// MongoDB
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
-export default app;
+// CommonJS export for Vercel
+module.exports = app;
